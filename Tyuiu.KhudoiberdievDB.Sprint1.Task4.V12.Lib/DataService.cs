d65ec5cce0b238fe45399b6842a3941ede2a424a
Tyuiu.HudojberdievDB@outlook.com <Tyuiu.HudojberdievDB@outlook.com>
2024-10-12 14:09:48 +0300

    Add project files.

---
+++ Tyuiu.KhudoiberdievDB.Sprint1.Task4.V12.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KhudoiberdievDB.Sprint1.Task4.V12.Lib
+{
+    public class DataService : ISprint1Task4V12
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = Math.Sin(x) / (x + (Math.Sqrt(Math.Abs(y))));
+            res = Math.Round(res, 3);
+            return res;
+
+        }
+    }
+}
