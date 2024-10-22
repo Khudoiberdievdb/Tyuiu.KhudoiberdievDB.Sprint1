dcdc0bd9f4277482a4ae06f8cd76e3a8a819ca40
Tyuiu.HudojberdievDB@outlook.com <Tyuiu.HudojberdievDB@outlook.com>
2024-10-22 17:06:18 +0300

    Task7

--- Tyuiu.KhudoiberdievDB.Sprint1.Task7.V30.Lib/DataService.cs
+++ Tyuiu.KhudoiberdievDB.Sprint1.Task7.V30.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KhudoiberdievDB.Sprint1.Task7.V30.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res = (x + Math.Pow(Math.E, x)) + (((Math.Sin(Math.Pow(x,5)) + Math.Pow(x, 3))) / (Math.Pow(3, x)) + (Math.Pow(y, 5)) / (Math.Pow(5, y));
+            double res = (x + Math.Pow(Math.E, x)) + (((Math.Sin(Math.Pow(x, 5))) + Math.Pow(x, 3))) / (Math.Pow(3, x)) + (Math.Pow(y, 5)) / (Math.Pow(5, y));
             return Math.Round(res,3);
 
 
